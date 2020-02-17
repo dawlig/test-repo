414dbcb3e36de5e00b5db8e58ca97067eb4a816e
dawidligas95@gmail.com <dawidligas95@gmail.com>
2020-02-17 14:54:54 +0100

    updated files

--- prev.c
+++ prev.c
@@ -1,1 +1,4 @@
 /*Prev file created ver1.0*/
+
+#define OK 0
+#define NOK 1
--- trial.h
+++ trial.h
@@ -1,5 +1,5 @@
 /*Headers file*/
 
-#define ADC_Value = 1.5 
-#define ADC_MIN = 0.5
-#define ADC_MAX = 2.5
+#define ADC_Value 1.5 
+#define ADC_MIN 0.5
+#define ADC_MAX 2.5
